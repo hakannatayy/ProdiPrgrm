e31da09cf6d8c31efef93314bdc35ec99ad192c2
hakannatayy <48553376+hakannatayy@users.noreply.github.com>
2019-06-16 17:24:47 +0300

    form işlemleri yapıldı.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,75 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(40, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(183, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(349, 121);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(200, 199);
+            this.listBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(229, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(687, 528);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,19 +2,131 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin;
 
 namespace Uygulama
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void getProcesses()
+        {
+            Process[] processes = Process.GetProcesses();
+
+            listBox1.Items.Clear();
+            foreach (Process process in processes)
+            {
+                listBox1.Items.Add(process.ProcessName);
+            }
+        }
+
+        private void killSelectedProcesses()
+        {
+            DialogResult dialogResult = MessageBox.Show("İşlemi sonlandırmak istiyor musunuz?", "Dikkat", MessageBoxButtons.YesNo);
+
+            if (dialogResult.Equals(DialogResult.Yes))
+            {
+                var processName = listBox1.SelectedItem;
+                Process[] processes = Process.GetProcessesByName(processName.ToString());
+
+                foreach (Process process in processes)
+                {
+                    Debug.WriteLine("İşlem sonlandırıldı: " + process.ProcessName);
+                    process.Kill();
+                }
+
+                MessageBox.Show("İşlem sonlandırıldı: " + processName, "Başarılı");
+            }
+        }
+
+        private void killBannedProcesses()
+        {
+            foreach (string processName in listBox2.Items)
+            {
+                Process[] processes = Process.GetProcessesByName(processName);
+
+                foreach (Process process in processes)
+                {
+                    Debug.WriteLine("İşlem sonlandırıldı: " + process.ProcessName);
+                    process.Kill();
+                }
+            }
+        }
+
+        private void setButtonsEnabledState(bool state)
+        {
+            button1.Enabled = state;
+            button2.Enabled = state;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaterialSkinManager msm = MaterialSkinManager.Instance;
+            msm.AddFormToManage(this);
+            msm.Theme = MaterialSkinManager.Themes.DARK;
+
+            this.setButtonsEnabledState(false);
+
+            timer1.Interval = 10000;
+            timer1.Start();
+
+            this.getProcesses();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            this.getProcesses();
+            this.killBannedProcesses();
+            this.setButtonsEnabledState(false);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.killSelectedProcesses();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string processName = listBox1.SelectedItem.ToString();
+
+            if (listBox2.Items.Contains(processName))
+            {
+                MessageBox.Show("Bu işlemi zaten yasakladınız.", "Hata");
+                return;
+            }
+
+            listBox2.Items.Add(processName);
+            this.killBannedProcesses();
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData.Equals(Keys.Delete))
+            {
+                this.killSelectedProcesses();
+            }
+        }
+
+        private void listBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData.Equals(Keys.Delete))
+            {
+                listBox2.Items.Remove(listBox2.SelectedItem);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.setButtonsEnabledState(listBox1.Items.Count > 0);
         }
     }
 }
